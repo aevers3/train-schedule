3c2701ae2c69d77ea650c51c0e2b374b7bd83411
aevers3 <aevers3@users.noreply.github.com>
2019-03-21 11:55:02 -0400

    added functional js

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -59,21 +59,54 @@ database.ref().on("child_added", function (childSnapshot) {
   var firstTrain = childSnapshot.val().newTrain.firstTrain;
   var frequency = childSnapshot.val().newTrain.frequency;
 
-  console.log(trainName);
-  console.log(destination);
-  console.log(firstTrain);
-  console.log(frequency);
-
-  // Create a variable for calculated arrival time
+  // console.log(trainName);
+  // console.log(destination);
+  // console.log(firstTrain);
+  // console.log(frequency);
 
 
+
+
+  // ******************** CALCULATED FIELDS (Next arrival time, minutes until next train) ********************
+
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+  // First train - push back 1 year so it's before the currentTime.
+  console.log('FIRST TRAIN: ' + firstTrain);
+  // ***** QUESTION for someone - In the line below, is 'HH:mm' used to 'catch' firstTrain in that format before converting to UNIX?
+  var firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+  console.log('FIRST TRAIN UNIX: ' + firstTrainConverted);
+
+  // Difference between firstTrain and currentTime...
+  var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+  console.log('DIFFTIME ' + diffTime);
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % frequency;
+  console.log('REMAINDER: ' + tRemainder);
+
+  // Minutes until next train... Returns as number (console.log below combines it into string - don't be fooled.)
+  var minutesTillTrain = frequency - tRemainder;
+  console.log('MINUTES TILL TRAIN: ' + minutesTillTrain);
+
+  var nextTrain = moment().add(minutesTillTrain, 'minutes');
+  console.log('NEXT TRAIN: ' + moment(nextTrain).format("hh:mm"));
+
+
+
+  // *********************** ADD TO TABLE ON SCREEN ***********************
+  // Build new table row and append all of the <td>'s
   var newRow = $('<tr>').append(
     $("<td>").text(''),
     $("<td>").text(trainName),
     $("<td>").text(destination),
-    $("<td>").text(firstTrain),
-    // $("<td>").text(frequency)
+    $("<td>").text(moment(nextTrain).format('hh:mm')),
+    $("<td>").text(minutesTillTrain),
   );
 
+  // Append newly populated row to DOM.
   $("#train-table > tbody").append(newRow);
+
 });
+
